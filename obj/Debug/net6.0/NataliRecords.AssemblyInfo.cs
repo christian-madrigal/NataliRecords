383ffe0920f2d8b92821a4cf414f2f9a41bc0611
christian-madrigal <98770177+christian-madrigal@users.noreply.github.com>
2025-01-23 13:08:05 -0800

    update

--- obj/Debug/net6.0/NataliRecords.AssemblyInfo.cs
+++ obj/Debug/net6.0/NataliRecords.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NataliRecords")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc347374c8003fe9f8cb46c61718a3e39fa7aedb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51e053490ecade35ec20d49db0ccbc461e3ad8d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("NataliRecords")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NataliRecords")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
